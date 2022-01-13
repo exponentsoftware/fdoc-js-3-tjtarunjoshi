8296eedf0a231a2311efc1685e9066fbf02bd9ca
tom <52026729+tjtarunjoshi@users.noreply.github.com>
2022-01-13 17:34:57 +0530

    Update file1.js

--- file1.js
+++ file1.js
@@ -1,1 +1,16 @@
+// Write a function called convertArrayToObject which can convert the array to a structured object.
 
+const students = [
+  ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]],
+  ["John", ["HTM", "CSS", "JS", "React"], [85, 80, 85, 80]],
+];
+
+let ans = [];
+function convertArrayToObject(students) {
+  students.forEach((student) => {
+    ans.push({ name: student[0], skills: student[1], scores: student[2] });
+  });
+  return ans;
+}
+
+console.log(convertArrayToObject(students));
